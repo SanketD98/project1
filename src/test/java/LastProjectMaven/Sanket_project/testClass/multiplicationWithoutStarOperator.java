825e52b84d3afc9c138ef2a0668fc25c757d0253
sanke <sanke@SANKET>
2022-07-29 15:42:54 +0530

    second commit

---
+++ src/test/java/LastProjectMaven/Sanket_project/testClass/multiplicationWithoutStarOperator.java
@@ -0,0 +1,16 @@
+package LastProjectMaven.Sanket_project.testClass;
+
+public class multiplicationWithoutStarOperator 
+{
+	public static void main(String []args)
+	{
+		int a=6;
+		int b=2;
+		int mult=0;
+		for(int i=1;i<=a;i++)
+		{
+			mult=mult+b;
+		}
+		System.out.println(mult);
+	}
+}
